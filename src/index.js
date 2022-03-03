f41102b8bea383be877093dba2bea2555c76498b
Robert Butler <robertbutler@protonmail.com>
2022-03-03 13:29:44 +0000

    inital commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { render } from 'react-dom';
+import { StrictMode } from 'react';
+import App from './components/App';
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
